0bcf3cc46bd09384121ee047387f107f1bc758fe
Anderson Madureira <anderson.madureira@viavarejo.com.br>
2019-06-07 20:39:33 -0300

    Add new machines

--- terraform/machines_private.tf
+++ terraform/machines_private.tf
@@ -1,6 +1,6 @@
 resource "aws_network_interface" "mac_a_1" { 
   subnet_id = "${aws_subnet.private_a.id}"
-  private_ips = ["10.0.1.20"] 
+  private_ips = ["10.0.5.10"] 
 }
 
 resource "aws_instance" "backend_a" {
@@ -15,5 +15,44 @@ resource "aws_instance" "backend_a" {
   tags = { 
     Name = "private"
   }
+}
 
+resource "aws_network_interface" "mac_b_1" { 
+  subnet_id = "${aws_subnet.private_b.id}"
+  private_ips = ["10.0.6.10"] 
+}
+
+resource "aws_instance" "backend_b" {
+  ami           = "ami-0c6b1d09930fac512"
+  instance_type = "t2.micro"
+  network_interface {
+    network_interface_id   = "${aws_network_interface.mac_b_1.id}"
+    device_index           = 0
+    delete_on_termination  = false
+  } 
+  key_name = "deployer-key"
+  tags = { 
+    Name = "private"
+  }
+}
+
+
+
+resource "aws_network_interface" "mac_c_1" { 
+  subnet_id = "${aws_subnet.private_c.id}"
+  private_ips = ["10.0.7.10"] 
+}
+
+resource "aws_instance" "backend_c" {
+  ami           = "ami-0c6b1d09930fac512"
+  instance_type = "t2.micro"
+  network_interface {
+    network_interface_id   = "${aws_network_interface.mac_c_1.id}"
+    device_index           = 0
+    delete_on_termination  = false
+  } 
+  key_name = "deployer-key"
+  tags = { 
+    Name = "private"
+  }
 }
